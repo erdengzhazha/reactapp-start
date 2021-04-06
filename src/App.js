07480b8e95ed09b19ffc27de887758b8564285f3
Xiu_Er <13813641925@163.com>
2021-04-06 09:25:37 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// import logo from './logo.svg';
 import React,{Component} from 'react'
 import './App.css';
-// import './Table';
-import Table from "./Table";
 
 // function App() {
 //   return (
@@ -25,51 +22,10 @@ import Table from "./Table";
 //   );
 // }
 class App extends Component{
-    state = {
-        characters: [
-            {
-            name: 'Charlie',
-            job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ],
-    }
-    removeCharacter = (index) => {
-        const {characters} = this.state;
-
-        this.setState(
-            {
-                    characters: characters.filter((characters, i) => {
-                            return i !== index;
-                    })
-            })
-    }
-
 
     render() {
-        // const heading = <h1 className="site-heading">Hello, React</h1>
-        // const characters = [
-        //
-        // ]
         return (
-            // <div className={"app"}>
-            //     <h1>Hello,React!</h1>
-            //     {heading}
-            // </div>,
-            <div className={"container"}>
-              <Table characterDate={this.state.characters} removeCharacter={this.removeCharacter}/>
-            </div>
+            <div></div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,30 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-
-// class App extends React.Component{
-//     render() {
-//         const heading = <h1 className="site-heading">Hello, React</h1>
-//
-//         return (
-//             <div className={"app"}>
-//                 <h1>Hello,React!</h1>
-//                 {heading}
-//             </div>
-//         );
-//     }
-// }
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
